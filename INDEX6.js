f4d6a8cca4bab12e2a0ff500dcd8cc2c8b13d80e
NASTY008 <127878876+NASTY008@users.noreply.github.com>
2024-10-27 00:44:09 +0300

    Create INDEX6.js

---
+++ INDEX6.js
@@ -0,0 +1,95 @@
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+body {
+    background-color: #faeaea;
+    font-family: sans-serif;
+}
+main {
+    background-color: #fab010;
+    padding: 1dvw;
+    width: 100%;
+    max-width: 420px;
+    margin: 20dvh auto;
+    border-radius: 10px;
+    color: #fff
+}
+main * {
+    margin: 1dvh 0;
+}
+#title {
+    font-size: 40px;
+    color: #fcdee8;
+}
+.input-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.input-container * {
+    height: 40px;
+    font-size: 20px;
+    border-color: #fcdee8;
+    padding: 0 1dvw;
+    border-radius: 10px;
+}
+.plan-container {
+    background-color: #fcdee8;
+    color: #000;
+    padding: 1dvw;
+    border-radius: 10px;
+}
+.plan-container .plan-fields {
+    font-size: 20px;
+}
+.plan-container .plan-fields input {
+    margin-right: 1dvw;
+}
+#list {
+    width: 100%;
+    max-width: 200px;
+}
+.count-6 {
+    padding: 1dvw;
+    font-size: 20px;
+    background-color: #fcdee8;
+}
+.count {
+    width: 100%;
+    max-width: 150px;
+    outline: none;
+    border: none;
+    border-radius: 10px;
+}
+#button {
+    width: 100%;
+    height: 40px;
+    background-color: #fcdee8;
+    color: #000;
+    font-size: 20px;
+    border: none;
+    outline: none;
+    transition: .4s ease-out;
+    border-radius: 10px;
+}
+.option {
+    width: 100%;
+}
+#errorlog {
+    display: none;
+    background-color: red;
+    color: #000;
+    font-size: 20px;
+    text-align: center;
+    padding: 1rem;
+    border-radius: 10px;
+}
+#answer {
+    background-color: #96faee;
+    color: #000;
+    font-size: 20px;
+    padding: 1rem;
+    border-radius: 10px;
+}
